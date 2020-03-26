6df5519fc591f9ed0b49d4374cd36b8aba389310
Tu Hu <UYHDK@leo-pharma.com>
2020-03-26 11:11:17 +0100

    Update imputeNA.R
    change the threshold to 2/3 of the min value

--- R/imputeNA.R
+++ R/imputeNA.R
@@ -1,16 +1,14 @@
-imputeNA <- function(data=...){
+imputeNA <- function(data = ...) {
     data_n <- data %>% dplyr::group_by(feature) %>% tidyr::nest()
-    impute_na_one <- function(data=...){
-      test_na <- data$intensity %>% is.na() %>% sum()
-      ifelse(test_na == 0,
-          return(data),
-          {na_index <- data$intensity %>% is.na()
-          intensity_min <- data$intensity %>% min(na.rm = TRUE)
-          data$intensity[na_index] <- runif(na_index %>% sum, 0, intensity_min)
-          return(data)}
-    )
-  }
-    data_n_impute <- data_n %>% dplyr::mutate(data_impute_na = map(data, impute_na_one)) %>% dplyr::select(-data) %>%
-    tidyr::unnest(cols = c(data_impute_na))
+    impute_na_one <- function(data = ...) {
+        test_na <- data$intensity %>% is.na() %>% sum()
+        ifelse(test_na == 0, return(data), {
+            na_index <- data$intensity %>% is.na()
+            intensity_min <- data$intensity %>% min(na.rm = TRUE)
+            data$intensity[na_index] <- runif(na_index %>% sum, 0, intensity_min*0.67)
+            return(data)
+        })
+    }
+    data_n_impute <- data_n %>% dplyr::mutate(data_impute_na = map(data, impute_na_one)) %>% dplyr::select(-data) %>% tidyr::unnest(cols = c(data_impute_na))
     return(data_n_impute)
 }
