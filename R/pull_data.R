e80b592dc1f44a369bc573ec37dfdcb789c04fea
Tu Hu <UYHDK@leo-pharma.com>
2020-03-27 15:56:08 +0100

    Create pull_data.R

---
+++ R/pull_data.R
@@ -0,0 +1,17 @@
+#' Pull pre-processed data and summary statistics based on the regular expression (naming pattern)
+#'
+#' @param pattern regular expression in sample_table
+#' @param data_table. data table
+#' @param sample_table. sample information table
+pull.data <- function(pattern = ...,
+                      data_table. = data_table,
+                      sample_table. = sample_table){
+  datalist <- list()
+  datalist$pattern <- pattern
+  datalist$index <- sample_table.$filename %>% str_detect(pattern) %>% which()
+  datalist$data <- data_table. %>% select(1:6, paste0("X", datalist$index))
+  datalist$intensity <- data_table. %>% select(paste0("X", datalist$index))
+  datalist$intensity_mean <- datalist$intensity %>% rowMeans(na.rm = TRUE)
+  datalist$prevalence <- 1 - (datalist$intensity %>% is.na() %>% rowMeans())
+  return(datalist)
+}
