823f1d0af3c61af443d855142da871f34e2f9e2e
Tu Hu <UYHDK@leo-pharma.com>
2020-04-14 14:03:44 +0200

    Update imputeNA.R

--- R/imputeNA.R
+++ R/imputeNA.R
@@ -41,20 +41,10 @@ imputeNA <- function(data=...,
             return(data.)
         })
     }
-    data_n_impute <- data_n %>% dplyr::mutate(data_impute_na = map(data, impute_na_one)) %>% dplyr::select(-data) %>% tidyr::unnest(cols = c(data_impute_na))
+    data_n_impute <- data_n %>% dplyr::mutate(data_impute_na = purrr::map(data, impute_na_one)) %>% dplyr::select(-data) %>% tidyr::unnest(cols = c(data_impute_na))
     return(data_n_impute)
 }
 
 
 
-#' Detect early and late eluting compounds (early- and late- birds)
-#'
-#' @param rt retention time in the numeric format
-#' @param limit_low lower limit of features to be removed
-#' @param limit_high higher limit of features to be removed
-#' @return \code{rt} between \code{limit_low} and \code{limit-high}
-detect.birds <- function(rt = ..., limit_low = 0.3, limit_high = 6.5) {
-    rt.df <- data.frame(rt) %>% mutate(feature = paste0("F", 1:length(rt))) %>% mutate(bird = ifelse((rt > limit_low) & (rt < limit_high), TRUE, FALSE))
-    birds_id <- rt.df %>% pull(bird) %>% as.logical()
-    return(rt.df)
-}
+
