7d13c3707794afb08a9c8fc901221ed0a99c7086
Tu Hu <UYHDK@leo-pharma.com>
2020-04-14 14:03:19 +0200

    Update calibrateBatch_inter.R

--- R/calibrateBatch_inter.R
+++ R/calibrateBatch_inter.R
@@ -14,18 +14,18 @@ calibrateBatch.inter.rlm <- function(data = ...,
   intensity <- rlang::enexpr(intensity)
 
   data_n <- data %>%
-    group_by(!! feature, !! batch) %>% nest() %>%
-    mutate(data_intra_calibrate = map(data, libra::calibrateBatch.intra.rlm, intensity= !! intensity)) %>%
+    dplyr::group_by(!! feature, !! batch) %>% tidyr::nest() %>%
+    dplyr::mutate(data_intra_calibrate = purrr::map(data, libra::calibrateBatch.intra.rlm, intensity= !! intensity)) %>%
     select(-data)
 
   data_n_intra <- data_n %>%
-    mutate(intra_batch_center=map(data_intra_calibrate, function(x)x[["center_intensity"]] %>% unique)) %>%
-    ungroup() %>% mutate(intra_batch_center = as.numeric(intra_batch_center))
+    dplyr::mutate(intra_batch_center= purrr::map(data_intra_calibrate, function(x)x[["center_intensity"]] %>% unique)) %>%
+    dplyr::ungroup() %>% dplyr::mutate(intra_batch_center = as.numeric(intra_batch_center))
 
   data_n_inter <- data_n_intra %>%
-    group_by(!! feature) %>% summarize(multi_batch_center = mean(intra_batch_center))
+    dplyr::group_by(!! feature) %>% dplyr::summarize(multi_batch_center = mean(intra_batch_center))
 
   data_n_intra_inter <- data_n_intra %>%
-    left_join(data_n_inter, by = rlang::as_string(feature)) %>% mutate(factor = intra_batch_center / multi_batch_center) %>%
-    unnest(cols = c(data_intra_calibrate)) %>% mutate(intensity_intra_inter_calibrated = intensity_intra_calibrated / factor)
+    dplyr::left_join(data_n_inter, by = rlang::as_string(feature)) %>% dplyr::mutate(factor = intra_batch_center / multi_batch_center) %>%
+    tidyr::unnest(cols = c(data_intra_calibrate)) %>% dplyr::mutate(intensity_intra_inter_calibrated = intensity_intra_calibrated / factor)
 }
