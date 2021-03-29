f45bcae5f13985ae6fcd14d643b4b4cea2637bb4
Tu Hu <UYHDK@leo-pharma.com>
2021-03-29 09:11:29 +0200

    Update calibrateBatch_inter.R

--- R/calibrateBatch_inter.R
+++ R/calibrateBatch_inter.R
@@ -1,11 +1,14 @@
 #' Inter batch calibration by robust linear modelling (rlm)
 #'
-#' @param data metabolomics in tidy long-format
+#' @param data metabolomics data in tidy long-format
 #' @param feature column name of feature (by default feature)
 #' @param batch column name of batch (by default batch)
 #' @param intensity column name of intensity (by default intensity)
 
-calibrateBatch.inter.rlm <- function (data = ..., feature = feature, batch = batch, intensity = intensity,
+calibrateBatch.inter.rlm <- function (data = ...,
+                                      feature = feature,
+                                      batch = batch,
+                                      intensity = intensity,
                                       group = ...)
 {
   feature <- rlang::enexpr(feature)
@@ -14,18 +17,20 @@ calibrateBatch.inter.rlm <- function (data = ..., feature = feature, batch = bat
   group_factor <- rlang::enexpr(group)
   data_n <- data %>% dplyr::group_by(!!batch, !!feature, !!group_factor) %>%
     tidyr::nest() %>% dplyr::mutate(data_intra_calibrate = purrr::map(data,
-                                                                      libra::calibrateBatch.intra.rlm.single.feature, intensity = !!intensity))
+                                                                      libra::calibrateBatch.intra.rlm.single.feature,
+                                                                      intensity = !!intensity))
   data_n_intra <- data_n %>% dplyr::mutate(intra_batch_center = data_intra_calibrate %>%
                                              unlist %>% mean) %>% dplyr::ungroup()
   data_n_inter <- data_n_intra %>% dplyr::group_by(!!feature, !! group_factor) %>%
     dplyr::summarize(multi_batch_center = mean(intra_batch_center))
   data_n_intra_inter <- data_n_intra %>% dplyr::left_join(data_n_inter,
-                                                          by = c(rlang::as_string(feature), rlang::as_string(group_factor))) %>% dplyr::mutate(factor = intra_batch_center/multi_batch_center) %>%
+                                                          by = c(rlang::as_string(feature), rlang::as_string(group_factor))) %>%
+    dplyr::mutate(factor = intra_batch_center/multi_batch_center) %>%
     tidyr::unnest(cols = c(data, data_intra_calibrate)) %>%
     dplyr::mutate(intensity_intra_inter_calibrated = intensity_intra_calibrated/factor)
   data_n_intra_inter_output <- data_n_intra_inter %>% dplyr::select(-intensity_intra_calibrated,
                                                                     -intra_batch_center, -multi_batch_center, -factor) %>%
-    dplyr::arrange(feature, batch, injection_sequence)
+    dplyr::arrange(rlang::as_string(feature), rlang::as_string(batch), injection_sequence)
   return(data_n_intra_inter_output)
 }
 
@@ -51,6 +56,6 @@ calibrateBatch.inter.rlm.group <- function(data = ...,
     dplyr::mutate(intensity_intra_inter_calibrated = intensity_intra_calibrated/factor)
   data_n_intra_inter_output <- data_n_intra_inter %>% dplyr::select(-intensity_intra_calibrated,
                                                                     -intra_batch_center, -multi_batch_center, -factor) %>%
-    dplyr::arrange(feature, batch, injection_sequence)
+    dplyr::arrange(rlang::as_string(feature), batch, injection_sequence)
   return(data_n_intra_inter_output)
 }
