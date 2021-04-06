907e9b069bf983817ca9dd30d179607d94fe4e5c
Tu Hu <UYHDK@leo-pharma.com>
2021-04-06 08:08:51 +0200

    Update calibrateBatch_inter.R

--- R/calibrateBatch_inter.R
+++ R/calibrateBatch_inter.R
@@ -8,23 +8,21 @@
 calibrateBatch.inter.rlm <- function (data = ...,
                                       feature = feature,
                                       batch = batch,
-                                      intensity = intensity,
-                                      group = ...)
+                                      intensity = intensity)
 {
   feature <- rlang::enexpr(feature)
   batch <- rlang::enexpr(batch)
   intensity <- rlang::enexpr(intensity)
-  group_factor <- rlang::enexpr(group)
-  data_n <- data %>% dplyr::group_by(!!batch, !!feature, !!group_factor) %>%
+  data_n <- data %>% dplyr::group_by(!!batch, !!feature) %>%
     tidyr::nest() %>% dplyr::mutate(data_intra_calibrate = purrr::map(data,
                                                                       libra::calibrateBatch.intra.rlm.single.feature,
                                                                       intensity = !!intensity))
   data_n_intra <- data_n %>% dplyr::mutate(intra_batch_center = data_intra_calibrate %>%
                                              unlist %>% mean) %>% dplyr::ungroup()
-  data_n_inter <- data_n_intra %>% dplyr::group_by(!!feature, !! group_factor) %>%
+  data_n_inter <- data_n_intra %>% dplyr::group_by(!!feature) %>%
     dplyr::summarize(multi_batch_center = mean(intra_batch_center))
   data_n_intra_inter <- data_n_intra %>% dplyr::left_join(data_n_inter,
-                                                          by = c(rlang::as_string(feature), rlang::as_string(group_factor))) %>%
+                                                          by = c(rlang::as_string(feature))) %>%
     dplyr::mutate(factor = intra_batch_center/multi_batch_center) %>%
     tidyr::unnest(cols = c(data, data_intra_calibrate)) %>%
     dplyr::mutate(intensity_intra_inter_calibrated = intensity_intra_calibrated/factor)
